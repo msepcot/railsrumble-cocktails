0600abf15fce31cead1a94c71bcd5ed67055dbad
Robert Schiemann <rschiemann@tuneins-MacBook-Pro-3.local>
2014-10-18 14:30:13 -0700

    Created cocktail partial. Added model assocations to cocktail and ingredient

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,9 @@
 class Cocktail < ActiveRecord::Base
   belongs_to :drinkware
 
+  has_many :recipes
+  has_many :ingredients, through: :recipes
+
   validates_presence_of :name
   validates_presence_of :instructions
   validates_presence_of :drinkware
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,8 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :group
 
+  has_many :recipes
+  has_many :cocktails, through: :recipes
+
   validates_presence_of :name
 end
