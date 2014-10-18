6a50639b2cf66e0be7b70165909c73b707a8cec5
Jesse Atkinson <jesse.atkinson@me.com>
2014-10-18 11:37:35 -0700

    Ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,72 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141018005704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bars", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "ingredient_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "bars", ["ingredient_id"], name: "index_bars_on_ingredient_id", using: :btree
+  add_index "bars", ["user_id"], name: "index_bars_on_user_id", using: :btree
+
+  create_table "cocktails", force: true do |t|
+    t.string   "name"
+    t.string   "image"
+    t.text     "instructions"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "favorites", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "cocktail_id"
+    t.integer  "order"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "favorites", ["cocktail_id"], name: "index_favorites_on_cocktail_id", using: :btree
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
+
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ingredients", force: true do |t|
+    t.string   "name"
+    t.string   "image"
+    t.integer  "group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ingredients", ["group_id"], name: "index_ingredients_on_group_id", using: :btree
+
+  create_table "recipes", force: true do |t|
+    t.integer  "cocktail_id"
+    t.integer  "ingredient_id"
+    t.string   "measurement"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "recipes", ["cocktail_id"], name: "index_recipes_on_cocktail_id", using: :btree
+  add_index "recipes", ["ingredient_id"], name: "index_recipes_on_ingredient_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
