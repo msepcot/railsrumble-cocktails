28a153a76817dc1ea51d821599000cd04c78018f
Michael Sepcot <michael.sepcot@gmail.com>
2014-10-19 00:04:33 -0700

    add labels and sort ingredients alphabetically on ID page

--- app/controllers/intelligent_design_controller.rb
+++ app/controllers/intelligent_design_controller.rb
@@ -1,6 +1,6 @@
 class IntelligentDesignController < ApplicationController
   def index
-    @ingredients = Ingredient.find_in_batches(batch_size: 6)
+    @ingredients = Ingredient.order(:name)
   end
 
   def concoct
